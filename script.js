7e097407cb0dcb37e00e62d9ddbefffc878ec0d3
aspwarm <144545692+aspwarm@users.noreply.github.com>
2023-11-29 23:20:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,2 @@
-function sendMessage() {
-  const userInput = document.getElementById('user-input').value;
-  if (userInput.trim() !== '') {
-    displayMessage('User', userInput);
-    // Call backend/API for AI response (not implemented here)
-    // For example, you can use OpenAI's GPT-3 API
-    // Handle the AI response and call displayMessage again
-  }
-  document.getElementById('user-input').value = '';
-}
-
-function displayMessage(sender, message) {
-  const chatDisplay = document.getElementById('chat-display');
-  const messageElement = document.createElement('div');
-  messageElement.textContent = `${sender}: ${message}`;
-  chatDisplay.appendChild(messageElement);
-  chatDisplay.scrollTop = chatDisplay.scrollHeight;
-}
+// JavaScript for dynamic content
+// Fetch user profile and recommended courses from the server
